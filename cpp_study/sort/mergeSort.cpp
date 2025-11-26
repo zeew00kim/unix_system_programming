1528a982d24918bbfc521b5dc75264b24888816b
zeew00 <tukzeew00@gmail.com>
2025-11-27 00:23:13 +0900

    fix: mergeSort.cpp

--- cpp_study/sort/mergeSort.cpp
+++ cpp_study/sort/mergeSort.cpp
@@ -1,49 +1,48 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <cmath>
+#include <ctime>
 using namespace std;
 
-auto merge(vector<int>& p, int left, int pivot, int right) -> void {
-    int i = left, j = pivot + 1, k = left;
-    vector<int> temp(p.size());
-    while (i <= pivot && j <= right) {
-        if (p.at(i) < p.at(j)) temp.at(k++) = p.at(i++);
-        else temp.at(k++) = p.at(j++);
-    }
-    while (i <= pivot) temp.at(k++) = p.at(i++);
-    while (j <= right) temp.at(k++) = p.at(j++);
-    for (i = left; i <= right; i++) p.at(i) = temp.at(i);
+void merge(vector<int>& p, int left, int mid, int right) {
+	int i = left, j = mid + 1, k = left;
+	vector<int> temp(p.size());
+
+	while (i <= mid && j <= right) {
+		if (p.at(i) < p.at(j)) { temp.at(k++) = p.at(i++); }
+		else { temp.at(k++) = p.at(j++); }
+	}
+	while (i <= mid) { temp.at(k++) = p.at(i++); }
+	while (j <= right) { temp.at(k++) = p.at(j++); }
+	for (i = left; i <= right; i++) { p.at(i) = temp.at(i); }
 }
 
-auto mergeSort(vector<int>& p, int left, int right) -> void {
-    if (left < right) {
-        int pivot = (left + right) / 2;
-        mergeSort(p, left, pivot);
-        mergeSort(p, pivot + 1, right);
-        merge(p, left, pivot, right);
-    }
+void mergeSort(vector<int>& p, int left, int right) {
+	if (left < right) {
+		int mid = (left + right) / 2;	// 상수 시간 1
+		mergeSort(p, left, mid);		// T(p.size() / 2)
+		mergeSort(p, mid+1, right);		// T(p.size() / 2)
+		merge(p, left, mid, right);		// 최악의 경우 (n - 1)번 반복
+	}
+	// 총 시간 복잡도 T(n) = 2T(n/2) + (n - 1) + 1 = O(n log n)
 }
 
 int main() {
 
-    int size;
-    vector<int> arr;
-    srand(time(nullptr));
+	vector<int> arr;
+	int size;
 
-    cout << "enter the size of vector : ";
-    cin >> size;
+	cout << "enter the size of vector : ";
+	cin >> size;
 
-    arr.resize(size);
+	arr.resize(size);
 
-    for (int i = 0; i < size; i++) {
-        arr.at(i) = rand() % 30 + 1;
-    }
+	for (int i = 0; i < size; i++) { arr.at(i) = rand() % 30 + 1; }
 
-    mergeSort(arr, 0, size - 1);
+	mergeSort(arr, 0, arr.capacity() - 1);
 
-    for (int data : arr) cout << data << " ";
-    cout << endl;
+	for (int data : arr) { cout << data << " "; } 
+	cout << endl;
 
-    return 0;
+	return 0;
 }
